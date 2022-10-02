bcbd7a7f1532afdb923b54028f52fc474b92f5aa
testGitst09 <st091150@student.spbu.ru>
2022-10-02 19:30:21 +0300

    Created a connection to a local Mysql database and added main app

--- Back/Back/settings.py
+++ Back/Back/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -63,9 +64,13 @@ WSGI_APPLICATION = "Back.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_db',
+        'USER': 'Admin',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
